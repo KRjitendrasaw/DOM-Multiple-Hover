4fecc89ed4ce261e5f764cc60a634994f90b7fcb
Jitendra Kumar Saw <92841343+KRjitendrasaw@users.noreply.github.com>
2023-09-17 19:27:04 +0530

    added

---
+++ script.js
@@ -0,0 +1,15 @@
+var elem = document.querySelectorAll(".elem");
+
+elem.forEach(function (val) {
+  console.log(val.childNodes[3]);
+  val.addEventListener("mouseenter", function () {
+    val.childNodes[3].style.opacity = 1;
+  });
+  val.addEventListener("mouseleave", function () {
+    val.childNodes[3].style.opacity = 0;
+  });
+
+  val.addEventListener("mousemove", function (row) {
+    val.childNodes[3].style.left = row.x + "px";
+  });
+});
